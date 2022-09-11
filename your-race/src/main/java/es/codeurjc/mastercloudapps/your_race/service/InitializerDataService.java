68da2fc4617428e20b54335199fae06eba860bfa
raquetelio <rtoscano@gmail.com>
2022-09-11 13:13:49 +0200

    Initialize Race New York Marathon

--- your-race/src/main/java/es/codeurjc/mastercloudapps/your_race/service/InitializerDataService.java
+++ your-race/src/main/java/es/codeurjc/mastercloudapps/your_race/service/InitializerDataService.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import es.codeurjc.mastercloudapps.your_race.repos.OrganizerRepository;
 import es.codeurjc.mastercloudapps.your_race.domain.Organizer;
 
+import es.codeurjc.mastercloudapps.your_race.repos.RaceRepository;
+import es.codeurjc.mastercloudapps.your_race.domain.Race;
+import es.codeurjc.mastercloudapps.your_race.domain.ApplicationPeriod;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
 import javax.annotation.PostConstruct;
 
 @Service
@@ -13,30 +20,39 @@ public class InitializerDataService {
     @Autowired
     private OrganizerRepository organizerRepository;
 
+    @Autowired
+    private RaceRepository raceRepository;
+
     @PostConstruct
     public  void init(){
 
-        initializeOrganizers();
-        initializeRaces();
-
-    }
-
-    private void initializeOrganizers(){
-
         this.organizerRepository.deleteAll();
+        this.raceRepository.deleteAll();
 
         Organizer organizer1 = new Organizer("New York Road Runners", "Running race organization","New");
-        Organizer organizer2 = new Organizer("La Legion", "Running race organization","New");
+        ApplicationPeriod applicationPeriod1 =  new ApplicationPeriod(LocalDateTime.of(2022, Month.JANUARY,1,0,0), LocalDateTime.of(2022, Month.OCTOBER,31,23,59));
 
-        this.organizerRepository.save(organizer1);
-        this.organizerRepository.save(organizer2);
+        Organizer organizer2 = new Organizer("La Legión", "Running race organization","New");
+
+
+        Race race1 = new Race ("New York City Marathon",
+                "The New York City Marathon is an annual marathon that courses through the five boroughs of New York City. " +
+                        "It is the largest marathon in the world, with 53,627 finishers in 2019 and 98,247 applicants for the 2017 race",
+                LocalDateTime.of(2022, Month.NOVEMBER,6, 9,0),
+                "New York, NY, USA",
+                42.195,
+                "Running",
+                applicationPeriod1,
+                organizer1
+                );
+
+
+
+                this.organizerRepository.save(organizer1);
+                this.organizerRepository.save(organizer2);
+
+                this.raceRepository.save(race1);
 
     }
-
-    privat void initializeRaces(){
-
-
-    }
-
 
 }
