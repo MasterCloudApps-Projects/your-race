3e65b75e5715dfdfd52cad8c9edce0617cad101d
raquetelio <rtoscano@gmail.com>
2022-09-11 14:45:00 +0200

    Query organization

--- scriptsBBDD.sql
+++ scriptsBBDD.sql
@@ -1,3 +1,23 @@
+// DML Statements
+select r.*, ap.*, o.* 
+from organizer o,
+race r 
+FULL outer join application_period ap
+ON r.application_period_id  = ap.id 
+where r.organizer_id = o.id 
+
+
+
+// DDL Statements
+
+drop table race cascade;
+drop table application_period;
+drop table organizer;
+drop table athlete cascade;
+drop table registration;
+drop table application;
+
+
 delete from organizer
 delete from race
 delete from application_period 
@@ -6,11 +26,3 @@ delete from application_period
 select * from organizer 
 select * from race
 select * from application_period ap 
-
-
-select r.*, ap.*, o.* 
-from organizer o,
-race r 
-FULL outer join application_period ap
-ON r.application_period_id  = ap.id 
-where r.organizer_id = o.id 
