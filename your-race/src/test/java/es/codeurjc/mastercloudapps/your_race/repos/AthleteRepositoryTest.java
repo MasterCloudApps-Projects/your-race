e54138dc9d0e70e85fe99d1eca7bb48c29093608
rafarex70 <rafarex70@hotmail.com>
2022-09-27 16:41:40 +0200

    incluir testcontainers en tests

---
+++ your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/repos/AthleteRepositoryTest.java
@@ -0,0 +1,21 @@
+package es.codeurjc.mastercloudapps.your_race.repos;
+
+import es.codeurjc.mastercloudapps.your_race.domain.Athlete;
+import es.codeurjc.mastercloudapps.your_race.AbstractDatabaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+class AthleteRepositoryTest extends AbstractDatabaseTest {
+    @Autowired
+    private AthleteRepository athleteRepository;
+
+    @Test
+    public void test() throws InterruptedException {
+        List<Athlete> athletas = athleteRepository.findAll();
+        assertEquals(athletas.size(), 3);
+    }
+}
