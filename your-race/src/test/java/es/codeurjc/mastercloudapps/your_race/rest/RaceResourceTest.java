e19b6322536b64bb4746efec19454aff426aa4aa
rafarex70 <rafarex70@hotmail.com>
2022-10-05 20:05:01 +0200

    cambio test de integración que no funciona en GH

--- your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/rest/RaceResourceTest.java
+++ your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/rest/RaceResourceTest.java
@@ -1,7 +1,70 @@
 package es.codeurjc.mastercloudapps.your_race.rest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.github.javafaker.Faker;
+import es.codeurjc.mastercloudapps.your_race.AbstractDatabaseTest;
+import es.codeurjc.mastercloudapps.your_race.model.RaceDTO;
+import es.codeurjc.mastercloudapps.your_race.service.RaceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-class RaceResourceTest {
+import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RaceResourceTest extends AbstractDatabaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private RaceService raceService;
+
+    @Test
+    void getRacesTest() throws Exception {
+
+        List<RaceDTO> races = Arrays.asList(RaceDTO.builder().name("Ronda").build(),RaceDTO.builder().name("NYC").build());
+
+        when(raceService.findAll()).thenReturn(races);
+
+        mvc.perform(get("/api/races")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", equalTo("Ronda")));
+
+    }
+
+    @Test
+    void createRaceTest() throws Exception {
+
+        RaceDTO race = RaceDTO.builder().name("Ronda").location("Ficticia").organizer(1L).build();
+
+        when(raceService.create(any(RaceDTO.class))).thenReturn(1L);
+
+        mvc.perform(post("/api/races")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(race)))
+                .andExpect(status().isCreated());
+
+    }
 }
