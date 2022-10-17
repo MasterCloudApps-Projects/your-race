4a7eb49e8422a93db837d7cc3fc2a3e66889084c
raquetelio <rtoscano@gmail.com>
2022-10-17 23:13:10 +0200

    Refactorization

--- your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/usecase/AthleteUseCaseTest.java
+++ your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/usecase/AthleteUseCaseTest.java
@@ -11,10 +11,7 @@ import es.codeurjc.mastercloudapps.your_race.repos.AthleteRepository;
 import es.codeurjc.mastercloudapps.your_race.repos.OrganizerRepository;
 import es.codeurjc.mastercloudapps.your_race.repos.RaceRepository;
 import es.codeurjc.mastercloudapps.your_race.service.RaceService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,9 +26,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -53,17 +50,79 @@ public class AthleteUseCaseTest extends AbstractDatabaseTest {
     @Autowired
     private ApplicationRepository applicationRepository;
 
-    private Faker faker;
+
     Organizer organizer;
 
+    List<Race> raceList;
+    List<Athlete> athleteList;
 
+    private static class initializerData {
+        private static Faker faker;
 
+        static void init(){
+            faker = new Faker();
+        }
+
+        static Race buildTestRace(Organizer organizer) {
+            return Race.builder()
+                    .name(faker.esports().event())
+                    .description("A race for testing")
+                    .location(faker.address().cityName())
+                    .distance(faker.number().randomDouble(2, 0, 1000))
+                    .organizer(organizer)
+                    .raceRegistration(Registration.builder()
+                            .registrationDate(LocalDateTime.now().plusMonths(4L))
+                            .build())
+                    .date(LocalDateTime.now().plusMonths(6L))
+                    .build();
+        }
+
+        static void setDateInFuture(Race race) {
+
+            LocalDateTime date = LocalDateTime.now().plusMonths(4L);
+            race.setDate(LocalDateTime.of(date.getYear(), date.getMonth(), 1, 9, 0));
+
+        }
+
+        static void setDateInPast(Race race) {
+
+            LocalDateTime date = LocalDateTime.now().minusMonths(1L);
+            race.setDate(LocalDateTime.of(date.getYear(), date.getMonth(), 1, 9, 0));
+
+        }
+
+        static Athlete buildTestAthlete() {
+            return Athlete.builder().name(faker.name().firstName()).surname(faker.name().lastName()).build();
+        }
+        static Organizer buildTestOrganizer(){
+            return Organizer.builder().name("Test Organizer").build();
+        }
+    }
 
     @BeforeEach
     public void initEach(){
-        faker = new Faker();
+
+        initializerData.init();
         applicationRepository.deleteAll();
         raceRepository.deleteAll();
+        organizerRepository.deleteAll();
+
+
+        raceList = new ArrayList<Race>();
+        athleteList = new ArrayList<Athlete>();
+
+        organizer = initializerData.buildTestOrganizer();
+
+        for (int i=0; i<3;i++)
+            raceList.add(initializerData.buildTestRace(organizer));
+
+
+        for (int i=0; i<2;i++)
+            athleteList.add(initializerData.buildTestAthlete());
+
+        organizerRepository.save(organizer);
+        raceRepository.saveAll(raceList);
+        athleteRepository.saveAll(athleteList);
 
     }
 
@@ -71,23 +130,11 @@ public class AthleteUseCaseTest extends AbstractDatabaseTest {
     @Test
     void shouldGetListOpenRaces() throws Exception{
 
-        Organizer organizer = Organizer.builder().name("Test Organizer").build();
-        organizerRepository.save(organizer);
+        initializerData.setDateInPast(raceList.get(0));
+        initializerData.setDateInFuture(raceList.get(1));
+        initializerData.setDateInFuture(raceList.get(2));
 
-        Race race = buildTestRace(organizer);
-        Race plannedRace1 = buildTestRace(organizer);
-        Race plannedRace2 = buildTestRace(organizer);
-
-
-        this.setDateInPast(race);
-        this.setDateInFuture(plannedRace1);
-        this.setDateInFuture(plannedRace2);
-
-        raceRepository.save(race);
-        raceRepository.save(plannedRace1);
-        raceRepository.save(plannedRace2);
-
-
+        raceRepository.saveAll(raceList);
 
         mvc.perform(get("/api/races")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -103,47 +150,13 @@ public class AthleteUseCaseTest extends AbstractDatabaseTest {
 
     }
 
-    Race buildTestRace(Organizer organizer){
-        return Race.builder()
-                .name(faker.esports().event())
-                .description("A race for testing")
-                .location(faker.address().cityName())
-                .distance(faker.number().randomDouble(2,0,1000))
-                .organizer(organizer)
-                .raceRegistration(Registration.builder()
-                        .registrationDate( LocalDateTime.now().plusMonths(4L))
-                        .build())
-                .date(LocalDateTime.now().plusMonths(6L))
-                .build();
-    }
-    void setDateInFuture(Race race){
 
-        LocalDateTime date = LocalDateTime.now().plusMonths(4L);
-        race.setDate(LocalDateTime.of(date.getYear(), date.getMonth(),1,9,0));
-
-    }
-
-    void setDateInPast(Race race){
-
-        LocalDateTime date = LocalDateTime.now().minusMonths(1L);
-        race.setDate(LocalDateTime.of(date.getYear(), date.getMonth(),1,9,0));
-
-    }
 
     @DisplayName("Existing athlete should apply to existing race")
     @Test
     void athleteShouldApplyToExistingRace() throws Exception{
 
-        Athlete athlete = Athlete.builder().name("Raquel").surname("Toscano").build();
-        athleteRepository.save(athlete);
-
-        Organizer organizer = Organizer.builder().name("Test Organizer").build();
-        organizerRepository.save(organizer);
-
-        Race race = buildTestRace(organizer);
-        raceRepository.save(race);
-
-       mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/"+race.getId())
+       mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/"+raceList.get(0).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
@@ -154,24 +167,16 @@ public class AthleteUseCaseTest extends AbstractDatabaseTest {
 
     }
 
+
     @DisplayName("Application to non-existing Athlete and/or race should not be possible")
     @Test
     void applicationToNonExistingAthleteRaceShouldNotBePossible() throws Exception{
 
-        Athlete athlete = Athlete.builder().name("Raquel").surname("Toscano").build();
-        athleteRepository.save(athlete);
-
-        Organizer organizer = Organizer.builder().name("Test Organizer").build();
-        organizerRepository.save(organizer);
-
-        Race race = buildTestRace(organizer);
-        raceRepository.save(race);
-
-        mvc.perform(post("/api/athletes/" + "0000" + "/application/"+race.getId())
+        mvc.perform(post("/api/athletes/" + "0000" + "/application/"+raceList.get(0).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
 
-        mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/" + "0000")
+        mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/" + "0000")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
 
@@ -184,47 +189,29 @@ public class AthleteUseCaseTest extends AbstractDatabaseTest {
     @Test
     void shouldGetAthleteApplicationRacesList() throws Exception {
 
-        Athlete athlete = Athlete.builder().name("Raquel").surname("Toscano").build();
-        athleteRepository.save(athlete);
-
-        Athlete athlete2 = Athlete.builder().name("Rafael").surname("Gómez").build();
-        athleteRepository.save(athlete2);
-
-        Organizer organizer = Organizer.builder().name("Test Organizer").build();
-        organizerRepository.save(organizer);
-
-        Race race1 = buildTestRace(organizer);
-        raceRepository.save(race1);
-
-        Race race2 = buildTestRace(organizer);
-        raceRepository.save(race2);
-
-        Race race3 = buildTestRace(organizer);
-        raceRepository.save(race3);
-
-        mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/"+race1.getId())
+        mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/"+raceList.get(0).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
 
-        mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/"+race2.getId())
+        mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/"+raceList.get(1).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
 
 
-        mvc.perform(post("/api/athletes/" + athlete2.getId()+"/application/"+race1.getId())
+        mvc.perform(post("/api/athletes/" + athleteList.get(1).getId() +"/application/"+raceList.get(1).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
 
 
-        mvc.perform(get("/api/athletes/" + athlete.getId()+"/applications")
+        mvc.perform(get("/api/athletes/" + athleteList.get(0).getId()+"/applications")
                         .param("open","false")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].raceName", is(race1.getName())));
+                .andExpect(jsonPath("$[0].raceName", is(raceList.get(0).getName())));
 
     }
 
@@ -233,54 +220,44 @@ public class AthleteUseCaseTest extends AbstractDatabaseTest {
     @Test
     void shouldGetAthleteApplicationOpenRacesList() throws Exception {
 
-        Athlete athlete = Athlete.builder().name("Raquel").surname("Toscano").build();
-        athleteRepository.save(athlete);
 
+        initializerData.setDateInPast(raceList.get(0));
+        initializerData.setDateInFuture(raceList.get(1));
+        initializerData.setDateInFuture(raceList.get(2));
 
-        Organizer organizer = Organizer.builder().name("Test Organizer").build();
-        organizerRepository.save(organizer);
+        raceRepository.saveAll(raceList);
 
-        Race race1 = buildTestRace(organizer);
-        Race race2 = buildTestRace(organizer);
-        Race race3 = buildTestRace(organizer);
-
-        this.setDateInPast(race1);
-        this.setDateInFuture(race2);
-        this.setDateInFuture(race3);
-
-        raceRepository.save(race1);
-        raceRepository.save(race2);
-        raceRepository.save(race3);
-
-        mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/"+race1.getId())
+        mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/"+raceList.get(0).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
 
-        mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/"+race2.getId())
+        mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/"+raceList.get(1).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
 
 
-        mvc.perform(post("/api/athletes/" + athlete.getId()+"/application/"+race3.getId())
+        mvc.perform(post("/api/athletes/" + athleteList.get(0).getId()+"/application/"+raceList.get(2).getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.applicationCode").isNotEmpty());
 
 
-        mvc.perform(get("/api/athletes/" + athlete.getId()+"/applications")
+        mvc.perform(get("/api/athletes/" + athleteList.get(0).getId()+"/applications")
                         .param("open","true")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)));
 
-        mvc.perform(get("/api/athletes/" + athlete.getId()+"/applications")
+        mvc.perform(get("/api/athletes/" + athleteList.get(0).getId()+"/applications")
                         .param("open","false")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(3)));
 
     }
+
+
 
  }
