e3c3e032e030556840ff33c25cf1dedd69f79611
raquetelio <rtoscano@gmail.com>
2022-10-20 19:37:09 +0200

    Start of race registration feature

---
+++ your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/usecase/AthleteRaceRegistrationTest.java
@@ -0,0 +1,171 @@
+package es.codeurjc.mastercloudapps.your_race.usecase;
+
+import com.github.javafaker.Faker;
+import es.codeurjc.mastercloudapps.your_race.AbstractDatabaseTest;
+import es.codeurjc.mastercloudapps.your_race.domain.*;
+import es.codeurjc.mastercloudapps.your_race.repos.*;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AthleteRaceRegistrationTest extends AbstractDatabaseTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    private OrganizerRepository organizerRepository;
+
+    @Autowired
+    private RaceRepository raceRepository;
+
+    @Autowired
+    private AthleteRepository athleteRepository;
+
+
+    @Autowired
+    private TrackRepository trackRepository;
+
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    Organizer organizer;
+    List<Race> raceList;
+    List<Athlete> athleteList;
+    List<Track> tracksList;
+
+    private static class initializerData {
+        private static Faker faker;
+
+        static void init(){
+            faker = new Faker();
+        }
+
+        static Race buildTestRace(Organizer organizer) {
+            return Race.builder()
+                    .name(faker.esports().event())
+                    .description("A race for testing")
+                    .location(faker.address().cityName())
+                    .distance(faker.number().randomDouble(2, 0, 1000))
+                    .organizer(organizer)
+                    .raceRegistration(Registration.builder()
+                            .registrationDate(LocalDateTime.now().plusMonths(4L))
+                            .build())
+                    .applicationPeriod(ApplicationPeriod.builder()
+                            .initialDate(LocalDateTime.now().minusMonths(1L))
+                            .lastDate(LocalDateTime.now().plusMonths(1L))
+                            .build())
+                    .date(LocalDateTime.now().plusMonths(6L))
+                    .build();
+        }
+
+        static void setDateInFuture(Race race) {
+
+            LocalDateTime date = LocalDateTime.now().plusMonths(4L);
+            race.setDate(LocalDateTime.of(date.getYear(), date.getMonth(), 1, 9, 0));
+
+        }
+
+        static void setDateInPast(Race race) {
+
+            LocalDateTime date = LocalDateTime.now().minusMonths(1L);
+            race.setDate(LocalDateTime.of(date.getYear(), date.getMonth(), 1, 9, 0));
+
+        }
+
+        static Athlete buildTestAthlete() {
+            return Athlete.builder().name(faker.name().firstName()).surname(faker.name().lastName()).build();
+        }
+        static Organizer buildTestOrganizer(){
+            return Organizer.builder().name("Test Organizer").build();
+        }
+
+        static Track buildTrack(Athlete athlete, Race race){
+            return Track.builder()
+                    .athlete(athlete)
+                    .race(race)
+                    .status(faker.options().option("REGISTERED","PARTICIPATED"))
+                    .registrationDate(race.getRaceRegistration().getRegistrationDate())
+                    .dorsal(RandomUtils.nextInt())
+                    .build();
+        }
+        static void setApplicationPeriodOpen(Race race){
+            race.getApplicationPeriod().setInitialDate(LocalDateTime.now().minusMonths(1L));
+            race.getApplicationPeriod().setLastDate(LocalDateTime.now().plusMonths(2));
+        }
+
+        static void setApplicationPeriodClosed(Race race){
+            race.getApplicationPeriod().setInitialDate(LocalDateTime.now().minusMonths(3L));
+            race.getApplicationPeriod().setLastDate(LocalDateTime.now().minusMonths(2));
+        }
+
+    }
+
+    @BeforeEach
+    public void initEach(){
+
+        initializerData.init();
+        trackRepository.deleteAll();
+        applicationRepository.deleteAll();
+        raceRepository.deleteAll();
+        organizerRepository.deleteAll();
+
+
+
+        raceList = new ArrayList<Race>();
+        athleteList = new ArrayList<Athlete>();
+        tracksList = new ArrayList<Track>();
+
+        organizer = initializerData.buildTestOrganizer();
+
+        for (int i=0; i<3;i++)
+            raceList.add(initializerData.buildTestRace(organizer));
+
+
+        for (int i=0; i<3;i++)
+            athleteList.add(initializerData.buildTestAthlete());
+
+        organizerRepository.save(organizer);
+        raceRepository.saveAll(raceList);
+        athleteRepository.saveAll(athleteList);
+
+
+    }
+    @DisplayName("An athlete with application should register to race")
+    @Test
+    void athleteShouldRegisterToRace() throws Exception
+    {
+        mvc.perform(post("/api/registrations/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"athlete\": \"" + athleteList.get(0).getId().toString()
+                        + "\", \" + applicationCode\": \"" + "\"" + "APPLICATION_CODE" + "\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$", hasSize(1)));
+
+
+                // + athleteList.get(0).getId() + "/applications/" +  + "/registrations")  )
+    }
+
+}
