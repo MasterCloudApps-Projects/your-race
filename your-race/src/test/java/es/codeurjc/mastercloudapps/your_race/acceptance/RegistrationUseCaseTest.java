4a4f276cfef603637a857981c0791e38bbcf03cd
raquetelio <rtoscano@gmail.com>
2022-10-30 09:18:33 +0100

    RED: assignDorsalNumber - registration to race returns a null value in dorsal

--- your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/acceptance/RegistrationUseCaseTest.java
+++ your-race/src/test/java/es/codeurjc/mastercloudapps/your_race/acceptance/RegistrationUseCaseTest.java
@@ -115,8 +115,8 @@ public class RegistrationUseCaseTest {
                 .andExpect(status().isCreated()).andReturn();
 
         TrackDTO trackDTO = mapper.readValue(result.getResponse().getContentAsString(), TrackDTO.class);
-      //  assertThat(trackDTO.getDorsal()).isNotNull();
-        assertThat(trackDTO.getClass()).hasSameClassAs(TrackDTO.class);
+        assertThat(trackDTO.getDorsal()).isNotNull();
+
 
     }
 
