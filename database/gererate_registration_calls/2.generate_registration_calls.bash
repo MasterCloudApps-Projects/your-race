1dabef77c6789e45b41dd7f58e5ec112ad3b44aa
raquetelio <rtoscano@gmail.com>
2022-11-02 22:48:30 +0100

    Massive calls script

--- database/gererate_registration_calls/2.generate_registration_calls.bash
+++ database/gererate_registration_calls/2.generate_registration_calls.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export BASE_URL = 'http://localhost:8080'
-export ENDPOINT = '/api/tracks/byorder'
-export CALL_TEXT = echo 'curl -X POST '$BASE_URL$ENDPOINT'-H ''content-type: application/json''  -d '
+BASE_URL="http://localhost:8080"
+ENDPOINT='/api/tracks/byorder'
+CALL_TEXT='curl -X POST '$BASE_URL$ENDPOINT' -H "content-type: application/json" -d '
 
+#CALL_TEXT='curl -X POST http://localhost:8080/api/tracks/byorder -H \'content-type: application/json\' -d '
 
-docker exec k8s_pgdb_1 psql -d racedb admin -t -A -F"," -c "select 'curl http://localhost:8080' || '/api/tracks/byorder' || ' -H ''content-type: application/json''  -d '  || '''{ ''applicationCode'':  ''' || application_code || '''}''' from application a" > your_race_registration_calls.csv
+docker exec k8s_pgdb_1 psql -d racedb admin -t -A -F"," -c "select '$CALL_TEXT''{ \"applicationCode\":  \"' || application_code || '\"}''' from application a" > your_race_registration_calls.csv
+
+echo "done"
--- database/gererate_registration_calls/_delete_basic_data.sql
+++ database/gererate_registration_calls/_delete_basic_data.sql
@@ -1,7 +1,9 @@
+delete from track where race_id >10000;
+delete from application where id >10000;	
 delete from race where id >10000;		
 delete from organizer where id >10000;	
 delete from application_period where id >10000;	
 delete from registration_info where id >10000;	
 delete from athlete where id >10000;	
-delete from application where id >10000;	
+
 
