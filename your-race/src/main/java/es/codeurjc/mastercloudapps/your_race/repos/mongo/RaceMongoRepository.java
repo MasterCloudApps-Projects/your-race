e7c425889f47dad0e628bc9da2cab7bcfc434048
raquetelio <rtoscano@gmail.com>
2022-11-10 20:44:45 +0100

    fe: adds mongo repository interface

---
+++ your-race/src/main/java/es/codeurjc/mastercloudapps/your_race/repos/mongo/RaceMongoRepository.java
@@ -0,0 +1,8 @@
+package es.codeurjc.mastercloudapps.your_race.repos.mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RaceMongoRepository //extends MongoRepository
+//
+{
+}
