885f4c93dc193d36d5fa7fca9035dc58c3004837
raquetelio <rtoscano@gmail.com>
2022-11-13 21:24:05 +0100

    Script for setup env

--- your_race_setup.bash
+++ your_race_setup.bash
@@ -35,7 +35,7 @@ kubectl port-forward service/pgdb 5555:5432 &
 kubectl port-forward service/grafana 3000:3000 &
 
 # Importar los datos de test performance a la BBDD:
-psql postgresql://admin:admin@localhost:5555/racedb -f db/export_test_data_20221104/export_202211041741.sql
+psql postgresql://admin:admin@localhost:5555/racedb -f db/postgres/export_test_data_20221104/export_202211041741.sql
 
 
 # BBDD Mongo
@@ -52,6 +52,9 @@ export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 export INGRESS_HOST=$(minikube ip)
 echo "Istio gateway ingress port:"
 echo "http://$INGRESS_HOST:$INGRESS_PORT"
+
+# Watch cluster deployments, services,..
+watch -n 1 kubectl get pod,deployment,service,horizontalpodautoscaler,ingresses,destinationrule,virtualservice
 
 
 echo "done"
