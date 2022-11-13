2a94cddd95014609e381596659e2429663cac58c
raquetelio <rtoscano@gmail.com>
2022-11-13 22:43:11 +0100

    Prueba script mongo: fichero a fichero

--- db/mongo/mongo_import.bash
+++ db/mongo/mongo_import.bash
@@ -2,11 +2,27 @@
 #!/bin/bash
 
 db=$1
+ip=$INGRESS_HOST
+path="db/mongo/export/"
 
-cd db/mongo/export
-for file in *.json;
- do c=${file#*exp_$db_}; 
-    c=${c%.json}; 
-    mongoimport --uri mongodb://root:password@192.168.49.2:27017/$db --collection "${c}" --file "${file}";
- done
+#cd db/mongo/export
+#for file in *.json;
+# do c=${file#*exp_$db_}; 
+#    c=${c%.json}; 
+#    mongoimport --uri mongodb://root:password@$ip:27017/$db --collection "${c}" --file "${file}";
+# done
 
+c="race"
+file=$path"exp_racedb_race.json"
+#mongoimport --authenticationDatabase= admin --uri mongodb://root:password@$ip:27017/$db --collection "${c}" --file "${file}";
+
+mongoimport --uri=mongodb://root:password@$ip:27017/$db --collection="race" --file=$file;
+
+
+c="athlete"
+file="exp_racedb_athlete.json"
+mongoimport --authenticationDatabase= admin --uri mongodb://root:password@$ip:27017/$db --collection "${c}" --file "${file}";
+
+c="application"
+file="exp_racedb_application.json"
+mongoimport --authenticationDatabase= admin --uri mongodb://root:password@$ip:27017/$db --collection "${c}" --file "${file}";
